fb824288b3721ad05e25fcd43275cf766614dd91
Payton Yao <payton.yao@gmail.com>
2022-02-21 01:07:53 +0800

    Bump version to 0.9.0 signifying almost-public-readiness

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 # import sys, os
 
-version = '0.1.0'
+version = '0.9.0'
 
 with open('README.rst') as description_file:
     long_description = description_file.read()
